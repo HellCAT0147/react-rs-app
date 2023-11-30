352916e36f05a157ff813f718d4f0b135f9ada38
HellCAT0147 <letscheckmail23@gmail.com>
2023-11-30 21:16:20 +0500

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Main from './pages/Main';
 import UncontrolledFormPage from './pages/UncontrolledForm';
 import ReactHookFormPage from './pages/ReactHookForm';
 import NotFoundPage from './pages/NotFound';
+import Header from './components/header/Header';
 
 const App: React.FC = (): JSX.Element => (
-  <Routes>
-    <Route index element={<Main />} />
-    <Route path="/uncontrolled" element={<UncontrolledFormPage />} />
-    <Route path="/hook" element={<ReactHookFormPage />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
+  <>
+    <Header />
+    <Routes>
+      <Route index element={<Main />} />
+      <Route path="/uncontrolled" element={<UncontrolledFormPage />} />
+      <Route path="/hook" element={<ReactHookFormPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </>
 );
 
 export default App;
