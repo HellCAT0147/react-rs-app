8970cf229c99f2bd3d6d5bbe3b4798fb525185fa
HellCAT0147 <letscheckmail23@gmail.com>
2023-12-01 23:26:01 +0500

    feat: add form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import Main from './pages/Main';
-import UncontrolledFormPage from './pages/UncontrolledForm';
-import ReactHookFormPage from './pages/ReactHookForm';
-import NotFoundPage from './pages/NotFound';
 import Header from './components/header/Header';
+import CustomRoutes from './components/routes/Routes';
 
 const App: React.FC = (): JSX.Element => (
   <>
     <Header />
-    <Routes>
-      <Route index element={<Main />} />
-      <Route path="/uncontrolled" element={<UncontrolledFormPage />} />
-      <Route path="/hook" element={<ReactHookFormPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <CustomRoutes />
   </>
 );
 
--- src/components/react-hook-form/ReactHookForm.tsx
+++ src/components/react-hook-form/ReactHookForm.tsx
@@ -1,9 +1,121 @@
+import { useState } from 'react';
+import { useAppSelector } from '../../store/hooks/typed-hooks';
 import styles from './ReactHookForm.module.sass';
+import { useForm } from 'react-hook-form';
 
-const ReactHookForm: React.FC = (): JSX.Element => (
-  <>
-    <h1 className={styles.heading}>React Hook Form</h1>
-  </>
-);
+const ReactHookForm: React.FC = (): JSX.Element => {
+  const { countries } = useAppSelector((state) => state.gifReducer);
+  const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+  const [countryMatches, setCountryMatches] = useState<string[]>([]);
+  const [countryText, setCountryText] = useState<string>();
+
+  const { register } = useForm();
+
+  const filterCountries = (text: string) => {
+    setCountryText(text);
+    if (!text.length) setCountryMatches([]);
+    else {
+      const matches = countries.filter((country) => {
+        const regex = new RegExp(`${text}`, 'gi');
+        return country.match(regex);
+      });
+      setCountryMatches(matches);
+    }
+  };
+
+  const setCountry = (country: string) => {
+    setCountryText(country);
+    setCountryMatches([]);
+  };
+
+  return (
+    <article className={styles.formContainer}>
+      <h1 className={styles.heading}>React Hook Form</h1>
+      <form>
+        <input
+          className={styles.formElement}
+          type="text"
+          placeholder="Name"
+          {...register('name')}
+        />
+        <input
+          className={`${styles.formElement} ${styles.hideArrows}`}
+          type="number"
+          placeholder="Age"
+          {...register('age')}
+        />
+        <input
+          className={styles.formElement}
+          type="email"
+          placeholder="Email"
+          {...register('email')}
+        />
+        <input
+          className={styles.formElement}
+          type="password"
+          placeholder="Password"
+          {...register('password')}
+        />
+        <input
+          className={styles.formElement}
+          type="password"
+          placeholder="Confirm password"
+          {...register('confirm')}
+        />
+        <select
+          className={`${styles.formElement} ${styles.selectMenu}`}
+          {...register('gender')}
+        >
+          <option value="f">Female</option>
+          <option value="m">Male</option>
+          <option value="o">Other</option>
+        </select>
+        <div className={`${styles.labelledInput} ${styles.reversed}`}>
+          <label htmlFor="terms">A Terms and Conditions agreement</label>
+          <input
+            className={styles.checkbox}
+            type="checkbox"
+            {...register('terms')}
+            id="terms"
+          />
+        </div>
+        <div className={styles.labelledInput}>
+          <label htmlFor="pic">Picture</label>
+          <input
+            className={styles.file}
+            type="file"
+            {...register('pic')}
+            id="pic"
+          />
+        </div>
+        <input
+          className={styles.formElement}
+          type="text"
+          value={countryText ? countryText : ''}
+          placeholder={`Country, e.g. ${randomCountry}`}
+          {...register('country')}
+          onChange={(e): void => filterCountries(e.target.value)}
+        />
+        <div className={styles.countries}>
+          {countryMatches &&
+            countryMatches.map((country, id) => {
+              return (
+                <p
+                  className={`${styles.formElement} ${styles.country}`}
+                  key={id}
+                  onClick={(e) => {
+                    if (e.target instanceof HTMLElement && e.target.textContent)
+                      setCountry(e.target.textContent);
+                  }}
+                >
+                  {country}
+                </p>
+              );
+            })}
+        </div>
+      </form>
+    </article>
+  );
+};
 
 export default ReactHookForm;
